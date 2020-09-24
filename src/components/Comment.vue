e3b8bdc0e83ce012e415f70bf1cb026985c965ce
jaxxu <jaxxu@hotmail.com>
2020-09-24 17:33:22 +0800

    Add nested comment component

---
+++ src/components/Comment.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="container" v-for="(item,index) in record" :key="index">
+      <div class="commentHeader">
+        <p><span class="bold">{{ item.fromUser }}</span> replies to <span class="bold">{{ item.toUser }}</span> : </p>
+      </div>
+      <div class="commentBody">
+        <p>{{ item.content }}</p>
+      </div>
+      <div class="commentFooter">
+        <span class="reply-btn" @click="handleClickReply">Reply</span>
+        <span style="color:#969696">{{ item.time }}</span>
+      </div>
+      <nestComment v-if="item.cList" :record="item.cList" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name: 'nestComment',
+    data() {
+      return {}
+    },
+    props: ["record"],
+    methods: {
+      handleClickReply(e) {   // Click on reply button
+        const parent = e.target.parentNode
+        console.log(parent)
+      }
+    }
+}
+</script>
+<style scoped>
+  .container {
+    background: #ffffff;
+    margin: 20px auto 10px;
+    padding: 10px 10px 10px 25px;
+    border-radius: 10px;
+  }
+  .container:hover {
+      box-shadow: 4px 4px 10px lightgrey;
+  }
+  .bold {
+      font-weight: 500;
+  }
+  .commentBody {
+      padding: 10px;
+      margin: 10px 0;
+      background: #f7f7f7;
+  }
+  .commentFooter {
+      display: flex;
+      justify-content: space-between;
+  }
+  .reply-btn {
+      color: rgb(56, 159, 255);
+  }
+  .reply-btn:hover {
+      cursor: pointer;
+      color: rgb(115, 187, 255);
+  }
+  .reply-box {
+    margin-left: 25px;
+  }
+  textarea {
+    width: 100%;
+    border: none;
+    background: #f7f7f7;
+  }
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,64 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>Nested Comment Components</h2>
+    <div style="width:80%;margin:auto">
+      <Comment :record="comments"/>
+    </div>
   </div>
 </template>
+<script>
+import Comment from '@/components/Comment.vue'
+export default {
+  data() {
+    return {
+      comments: [
+        {
+          fromUser: "Alice",
+          toUser: "Bob",
+          content: "Hey, how is it going?",
+          time: "2020-09-24 13:45",
+          cList: [
+            {
+              fromUser: "Bob",
+              toUser: "Alice",
+              content: "Hey, I am doing well.",
+              time: "2020-09-24 13:49",
+              cList: [
+                {
+                  fromUser: "Alice",
+                  toUser: "Bob",
+                  content: "It's good to hear that.",
+                  time: "2020-09-24 13:55",
+                }
+              ]
+            }
+          ]
+        },
+        {
+          fromUser: "Alice",
+          toUser: "Bob",
+          content: "Hey, how is it going?",
+          time: "2020-09-24 13:45",
+        },
+        {
+          fromUser: "Alice",
+          toUser: "Bob",
+          content: "Hey, how is it going?",
+          time: "2020-09-24 13:45",
+          cList: [
+            {
+              fromUser: "Bob",
+              toUser: "Alice",
+              content: "Hey, I am doing well.",
+              time: "2020-09-24 13:49"
+            }
+          ]
+        },
+      ]
+    }
+  },
+  components: {
+    Comment
+  }
+}
+</script>
